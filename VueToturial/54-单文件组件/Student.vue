adcd69e94697220be66344d73ff78d326912dae3
Yukuncn <782885079@qq.com>
2021-12-20 23:14:15 +0800

    组件全部完成

---
+++ VueToturial/54-单文件组件/Student.vue
@@ -0,0 +1,28 @@
+//组件的结构
+<template>
+ <div>
+			<h2>学生名称：{{name}}</h2>
+            <h2>学生年龄：{{age}}</h2>
+			<button @click="showName">点我提示学校名</button>
+			</div>
+</template>
+
+//组件交互相关的代码（数据 方法 等等）
+<script>
+export default {
+	        name:"Student",
+			data:function(){
+				return{
+					name:"张三",
+					age:17
+				}
+			},
+			methods: {
+			   showName(){
+				console.log(this.name)
+			}	
+			},
+		}
+
+</script>
+
